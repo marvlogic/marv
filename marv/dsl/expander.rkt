50a7dbc4209eb46d527a0f032e2d949e87fa4631
Kevin Haines <5947118+kbhaines@users.noreply.github.com>
2023-08-27 11:44:18 +0100

    that's better!

--- marv/dsl/expander.rkt
+++ marv/dsl/expander.rkt
@@ -2,29 +2,39 @@
 
 (require (for-syntax racket/base syntax/parse))
 
+(begin-for-syntax
+  (define (c-marv-spec stx)
+    (displayln stx)
+    (syntax-parse stx
+      [(_ DECL ...) #'(begin DECL ...)]
+      [else (raise "nowt")]))
+
+  (define (c-decl stx)
+    (displayln stx)
+    (syntax-parse stx
+      [(_ SYM) #'(displayln SYM)]
+      [else (raise "nowt-decl")]))
+
+  (define (c-var-decl stx)
+    (syntax-parse stx
+      [(_ id EXPR) #'(displayln (format "name: ~a ~a" id EXPR))]
+      [else (raise "nowt-var-decl")]))
+
+  (define (c-expression stx)
+    (displayln stx)
+    (syntax-parse stx
+      [(_ val) #'val]
+      [else (raise "nowt-expression")]))
+
+  ; (require racket/pretty)
+  ;  (marv-spec (decl 'xyz) (decl 'pdq))
+  ; (marv-spec (decl 'xyz) (decl 'pdq))
+  ; (pretty-print (marv-spec (decl 'xyz) (decl 'pdq)))
+  )
+
+(define-syntax marv-spec c-marv-spec)
+(define-syntax decl c-decl)
+(define-syntax var-decl c-var-decl)
+(define-syntax expression c-expression)
+
 (provide marv-spec decl var-decl expression)
-
-(define-syntax (marv-spec stx)
-  (syntax-parse stx
-    [(_ DECL ...) #'(begin DECL ...)]
-    [else (raise "nowt")]))
-
-(define-syntax (decl stx)
-  (syntax-parse stx
-    [(_ SYM) #'(displayln SYM)]
-    [else (raise "nowt-decl")]))
-
-(define-syntax (var-decl stx)
-  (syntax-parse stx
-    [(_ id EXPR) #'(displayln (format "name: ~a ~a" id EXPR))]
-    [else (raise "nowt-var-decl")]))
-
-(define-syntax (expression stx)
-  (syntax-parse stx
-    [(_ val) #'val]
-    [else (raise "nowt-expression")]))
-
-; (require racket/pretty)
-;  (marv-spec (decl 'xyz) (decl 'pdq))
-; (marv-spec (decl 'xyz) (decl 'pdq))
-; (pretty-print (marv-spec (decl 'xyz) (decl 'pdq)))
