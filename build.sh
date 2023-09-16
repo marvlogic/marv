932b1297e41a4b2bef136fe14735d656407748a4
Kevin Haines <5947118+kbhaines@users.noreply.github.com>
2023-09-16 10:32:28 +0100

    fix docker build

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+set -e
 TAG=${1:-latest}
 BUILD=`mktemp -d`
 echo "Building in $BUILD"
 
 cp Dockerfile marv.sh $BUILD
-tar zcf $BUILD/marv.tgz alpha/ command.rkt core/ drivers/ examples/ info.rkt main.rkt utils/
+tar zcf $BUILD/marv.tgz alpha/ command.rkt core/ drivers/ examples/ info.rkt main.rkt log.rkt utils/
 docker build -t happyrat/marv:$TAG $BUILD
 rm -rf $BUILD
