c70a6e5517e001c78df75ba6ed13eefee3df1825
Kevin Haines <kevin@marvlogic.com>
2024-06-13 14:34:30 +0100

    updated build script

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ echo "Building in $BUILD"
 git clone . $BUILD
 cd $BUILD
 # cp Dockerfile marv.sh $BUILD
-tar zcf marv.tgz alpha/ command.rkt core/ drivers/ examples/ info.rkt main.rkt log.rkt utils/
+tar zcf marv.tgz alpha/ command.rkt core/ drivers/ examples/ info.rkt main.rkt log.rkt types/ utils/
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
 docker build -t marvlogic/marv:$TAG $BUILD
 rm -rf $BUILD
